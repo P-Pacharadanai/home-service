21b787d2e6e17ad3649b5452e16b8d02e6dcf67a
Hazuki <okubo.mae@gmail.com>
2024-02-13 00:23:38 +0900

    feat: configure client to integrate with supabase API

--- client/src/components/common/UserAccount.jsx
+++ client/src/components/common/UserAccount.jsx
@@ -1,36 +1,37 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { UserAcct, UserIconChange } from "../../constants";
+import { UserAcct, UserInfor } from "../../constants";
 
 const UserAccount = () => {
   const [activeItem, setActiveItem] = useState({ text: "", path: "" }); // Default active item
 
   const handleClick = (text, path) => {
-    setActiveItem({ text: text, path: path }); // Update active item state with clicked item
+    setActiveItem({
+      text,
+      path,
+    });
   };
 
   return (
-    <div className="h-[200px] bg-white rounded-lg border border-gray-300 w-52 dark:bg-white font-prompt">
+    <div className="h-[200px] bg-white rounded-lg border border-gray-300 w-[250px] dark:bg-white font-prompt">
       <div className="px-5 py-4 text-sm dark:text-white">
         <p className="text-gray-700 text-lg justify-start flex">บัญชีผู้ใช้</p>
         <hr className="border-gray-300 mt-4" />
       </div>
       <ul className="text-sm text-gray-300 dark:text-gray-950">
-        {UserIconChange.map((UserInfor, index) => (
-          <li key={index} >
-            <Link
-              to={{ pathname: UserInfor.path, state: UserInfor.text }}
-              onClick={() => handleClick(UserInfor.text, UserInfor.path)}
-              className={`flex items-center gap-4 ${UserInfor.path === activeItem.path ? 'active' : ''}`}>
+        {UserInfor.map((item, index) => (
+          <li key={index}>
+            <Link 
+              to={{ pathname: item.path, state: item.text }}
+              onClick={() => handleClick(item.text, item.path)}
+              className={`flex items-center gap-4 ${item.path === activeItem.path && item.text === activeItem.text ? 'active' : ''}`}>
               <img 
-                src={UserInfor.text === activeItem.text ? UserInfor.icon : UserAcct.find(item => item.text === UserInfor.text)?.icon}
+                 src={item.text === activeItem.text ? UserInfor.find(user => user.text === item.text)?.icon : UserAcct.find(user => user.text === item.text)?.icon}
                 alt="icon"
-                className={`ml-5 w-[22px] h-[24px] me-1 p-1  ${UserInfor.text === activeItem.text ? 'bg-gray-100' : 'dark:hover:text-gray-500'}`}
+                className={`ml-5 w-[22px] h-[24px] me-1 p-1  ${item.text === activeItem.text ? 'bg-gray-100' : 'dark:hover:text-gray-500'}`}
               />
-              <span
-                className={`block py-2 hover:bg-gray-100 dark:hover:bg-base ${UserInfor.text === activeItem.text ? 'text-blue-700' : 'dark:hover:text-gray-500'}`}
-              >
-                {UserInfor.text}
+              <span className={`block py-2 hover:bg-gray-100 dark:hover:bg-base ${item.text === activeItem.text ? 'text-blue-700' : 'dark:hover:text-gray-500'}`} >
+                {item.text}
               </span>
             </Link>
           </li>
