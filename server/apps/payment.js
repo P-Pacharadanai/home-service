4e3f9468fe547d800b7e44dafbb30f3d533c14f2
Pacharadanai <pacharadanaipinake@gmail.com>
2024-02-15 14:53:01 +0700

    feat: update payment feature

--- server/apps/payment.js
+++ server/apps/payment.js
@@ -4,29 +4,14 @@ import stripe from "stripe";
 const stripeInstance = stripe(process.env.STRIPE_SECRET_KEY);
 const paymentRouter = Router();
 
-const calculateOrderAmount = (data) => {
-  //   const amount = data.reduce(
-  //     (acc, curr) => (acc += curr.price * curr.quantity),
-  //     0
-  //   );
-  return 10;
-};
-
 paymentRouter.post("/create", async (req, res) => {
-  const data = req.body;
-  console.log("data:", data);
-
+  const { totalOrderPrice } = req.body;
   try {
     // Create a PaymentIntent with the order amount and currency
     const paymentIntent = await stripeInstance.paymentIntents.create({
       payment_method_types: ["promptpay", "card"],
-      //   payment_method_types: ["promptpay"],
-      // payment_method_types: ["card", "link"],
-      amount: calculateOrderAmount(data) * 100,
+      amount: totalOrderPrice * 100,
       currency: "thb",
-      //   automatic_payment_methods: {
-      //     enabled: true,
-      //   },
     });
 
     return res.json({
