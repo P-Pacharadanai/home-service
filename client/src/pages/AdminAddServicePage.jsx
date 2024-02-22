11c66f2bf456abb676c4be5c63ac237c08161494
Gravizz <peeke0rr@gmail.com>
2024-02-22 16:30:52 +0700

    style:add some ui

--- client/src/pages/AdminAddServicePage.jsx
+++ client/src/pages/AdminAddServicePage.jsx
@@ -2,9 +2,14 @@ import SidebarNavAdmin from "../components/common/SidebarNavAdmin";
 import { Select } from "@chakra-ui/react";
 import { useState, useEffect } from "react";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import supabase from "../utils/supabaseClient.js";
+import { GripVerticalIcon } from "../assets/icons/index.js";
 
 function AdminAddServicePage() {
+  const [serviceName, setServiceName] = useState("");
   const [subService, setSubService] = useState([]);
+  const [image, setImage] = useState(null);
+  const [category, setCategory] = useState("");
 
   const addSubService = () => {
     const newSubServiceItem = {
@@ -19,6 +24,10 @@ function AdminAddServicePage() {
 
   const removeSubService = (id) => {
     setSubService(subService.filter((item) => item.id !== id));
+  };
+
+  const handleServiceNameChange = (e) => {
+    setServiceName(e.target.value);
   };
 
   const handleDragEnd = (result) => {
@@ -40,11 +49,40 @@ function AdminAddServicePage() {
     setSubService(updatedSubService);
   };
 
+  const uploadImage = async () => {
+    try {
+      const { data, error } = await supabase.storage
+        .from("image")
+        .upload(`services-image/${image.name}`, image, {
+          cacheControl: "3600", // ตั้งค่า cache control ตามที่ต้องการ
+        });
+
+      if (error) {
+        console.error("Error uploading image:", error.message);
+      } else {
+        console.log("Image uploaded successfully:", data);
+        // ทำอย่างอื่นต่อไปที่คุณต้องการ
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleImageChange = async (e) => {
+    const file = e.target.files[0];
+    setImage(file);
+  };
+
+  const handleCreate = async () => {
+    if (image) {
+      await uploadImage();
+    }
+  };
+
   useEffect(() => {
     addSubService();
   }, []);
 
-  console.log(subService);
   return (
     <div className="flex h-screen font-prompt">
       <div className="h-full">
@@ -58,7 +96,10 @@ function AdminAddServicePage() {
               <button className="bg-blue-600 text-white rounded-lg px-6 py-2.5 ml-3">
                 ยกเลิก
               </button>
-              <button className="bg-blue-600 text-white rounded-lg px-6 py-2.5 ml-3">
+              <button
+                className="bg-blue-600 text-white rounded-lg px-6 py-2.5 ml-3"
+                onClick={handleCreate}
+              >
                 สร้าง
               </button>
             </div>
@@ -66,18 +107,25 @@ function AdminAddServicePage() {
           <section className="h-screen w-full bg-gray-100 p-10">
             <div className="flex flex-col gap-10 w-full h-full bg-white px-8 py-10 ">
               <div className="flex flex-row">
-                <p className="w-[200px]">ชื่อบริการ</p>
+                <p className="w-[200px]">
+                  ชื่อบริการ<span className="text-red">*</span>
+                </p>
                 <input
                   type="text"
                   placeholder="Enter service name"
-                  className="border "
+                  className="border border-gray-300 focus:outline-none rounded-lg w-[422px] h-[42px] "
+                  onChange={(e) => {
+                    handleServiceNameChange(e);
+                  }}
                 />
               </div>
               <div className="flex flex-row w-[400px]">
-                <p className="w-[200px]">หมวดหมู่</p>
+                <p className="w-[200px]">
+                  หมวดหมู่<span className="text-red">*</span>
+                </p>
                 <Select
                   variant="unstyled"
-                  placeholder="บริการทั้งหมด"
+                  placeholder="เลือกหมวดหมู่"
                   className="text-center "
                 >
                   <option value="ทั่วไป">บริการทั่วไป</option>
@@ -86,10 +134,12 @@ function AdminAddServicePage() {
                 </Select>
               </div>
               <div className="flex flex-row">
-                <p>รูปภาพ</p>
-                <input></input>
+                <p className="w-[200px]">
+                  รูปภาพ<span className="text-red">*</span>
+                </p>
+                <input type="file" onChange={handleImageChange}></input>
               </div>
-              <div className="flex flex-col gap-10">
+              <div className="flex flex-col gap-5">
                 <p>รายการบริการย่อย</p>
                 <Droppable droppableId="subServiceList">
                   {(provided) => (
@@ -105,15 +155,21 @@ function AdminAddServicePage() {
                               ref={provided.innerRef}
                               {...provided.draggableProps}
                               {...provided.dragHandleProps}
-                              className="flex flex-row gap-5"
+                              className="flex flex-row gap-10 my-5"
                             >
-                              <div className="w-[10px]">{index + 1}</div>
+                              <div className="flex justify-center items-center w-[20px]">
+                                <img
+                                  src={GripVerticalIcon}
+                                  className="w-[10px] h-full"
+                                />
+                              </div>
+
                               <div>
                                 <p>ชื่อรายการ</p>
                                 <input
                                   type="text"
                                   placeholder="Enter service name"
-                                  className="border w-[400px]"
+                                  className="border border-gray-300 focus:outline-none rounded-lg w-[422px] h-[42px] "
                                   onChange={(e) =>
                                     handleSubServiceChange(
                                       index,
@@ -128,7 +184,7 @@ function AdminAddServicePage() {
                                 <input
                                   type="text"
                                   placeholder="฿"
-                                  className="text-right border "
+                                  className="border border-gray-300 focus:outline-none rounded-lg w-[240px] h-[44px] "
                                   onChange={(e) =>
                                     handleSubServiceChange(
                                       index,
@@ -142,8 +198,7 @@ function AdminAddServicePage() {
                                 <p>หน่วยการบริการ</p>
                                 <input
                                   type="text"
-                                  placeholder="AAAAAAAAA"
-                                  className="border "
+                                  className="border border-gray-300 focus:outline-none rounded-lg w-[240px] h-[42px] "
                                   onChange={(e) =>
                                     handleSubServiceChange(
                                       index,
@@ -153,7 +208,10 @@ function AdminAddServicePage() {
                                   }
                                 />
                               </div>
-                              <a onClick={() => removeSubService(item.id)}>
+                              <a
+                                className="flex items-center "
+                                onClick={() => removeSubService(item.id)}
+                              >
                                 ลบรายการ
                               </a>
                             </div>
@@ -169,7 +227,7 @@ function AdminAddServicePage() {
                     onClick={addSubService}
                     className="bg-blue-600 text-white rounded-lg px-6 py-2.5 ml-3"
                   >
-                    เพิ่มรายการ
+                    เพิ่มรายการ +
                   </button>
                 </div>
               </div>
