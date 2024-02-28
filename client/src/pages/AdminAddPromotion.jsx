c31fa40389169f4a503247fdb3a47849964934e1
iHEDISNICE <hedis_nice@yahoo.com>
2024-02-28 14:18:23 +0700

    fix : code error

--- client/src/pages/AdminAddPromotion.jsx
+++ client/src/pages/AdminAddPromotion.jsx
@@ -8,6 +8,24 @@ import { useState } from "react";
 import axios from "axios";
 
 const AdminAddPromotion = () => {
+  const [promotionData, setPromotionData] = useState({});
+  const handleCreatePromotion = async () => {
+    const { data } = await axios.post(
+      `${import.meta.env.VITE_APP_HOME_SERVICE_API}/promotion`,
+      promotionData
+    );
+    console.log(promotionData);
+    navigate(`/admin-promotion/${data?.data?.id}`);
+  };
+
+  const [promotionCode, setPromotionCode] = useState(""); // State for promotion code
+  const [promotionType, setPromotionType] = useState(""); // State for promotion type
+  const [fixedDiscount, setFixedDiscount] = useState("");
+  const [percentDiscount, setPercentDiscount] = useState("");
+  const [usageLimit, setUsageLimit] = useState(""); // State for usage limit
+  const [expirationDate, setExpirationDate] = useState(null); // State for expiration date
+  const [expirationTime, setExpirationTime] = useState(null); // State for expiration time
+
   const navigate = useNavigate();
   return (
     <div className="flex h-screen ">
@@ -24,7 +42,22 @@ const AdminAddPromotion = () => {
         />
 
         <div className="flex-1 p-8  overflow-y-auto bg-base relative">
-          <AddPromotionDetail />
+          <AddPromotionDetail
+            promotionCode={promotionCode}
+            setPromotionCode={setPromotionCode}
+            promotionType={promotionType}
+            setPromotionType={setPromotionType}
+            fixedDiscount={fixedDiscount}
+            setFixedDiscount={setFixedDiscount}
+            percentDiscount={percentDiscount}
+            setPercentDiscount={setPercentDiscount}
+            usageLimit={usageLimit}
+            setUsageLimit={setUsageLimit}
+            expirationDate={expirationDate}
+            expirationTime={expirationTime}
+            setExpirationDate={setExpirationDate}
+            setExpirationTime={setExpirationTime}
+          />
         </div>
       </div>
     </div>
