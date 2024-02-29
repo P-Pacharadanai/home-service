da1727a7e9d3e804a7bc0edcbb369fcf74a92fef
iHEDISNICE <hedis_nice@yahoo.com>
2024-02-29 10:11:11 +0700

    style : some ui

--- client/src/components/adminPromotion/EditPromotionNav.jsx
+++ client/src/components/adminPromotion/EditPromotionNav.jsx
@@ -1,6 +1,6 @@
 import { ChevronLeft } from "lucide-react";
 
-const EditPromotionNav = () => {
+const EditPromotionNav = (props) => {
   return (
     <nav className="flex items-center w-full border-b p-4 bg-white font-prompt">
       <div>
@@ -16,11 +16,17 @@ const EditPromotionNav = () => {
         </h2>
       </div>
       <div className="w-full flex justify-end px-12 gap-4 ">
-        <button className="bg-white text-blue-600 text-lg px-9 py-2 rounded-lg border-blue-600 border">
-          ยกเลิก
+        <button
+          className="bg-white text-blue-600 text-lg px-9 py-2 rounded-lg border-blue-600 border"
+          onClick={props.onClickButtonCancel}
+        >
+          {props.buttonCancel}
         </button>
-        <button className="bg-blue-600 text-white text-lg px-9 py-2 rounded-lg">
-          แก้ไข
+        <button
+          className="bg-blue-600 text-white text-lg px-9 py-2 rounded-lg"
+          onClick={props.handleEditPromotion}
+        >
+          {props.buttonAdd}
         </button>
       </div>
     </nav>
--- client/src/pages/AdminEditPromotion.jsx
+++ client/src/pages/AdminEditPromotion.jsx
@@ -1,7 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import { SidebarNavAdmin } from "../components/common/";
 import { EditPromotionNav } from "../components/adminPromotion";
 import { AddPromotionDetail } from "../components/adminAddPromotion";
+
 const AdminEditPromotion = () => {
+  const navigate = useNavigate();
   return (
     <div className="flex h-screen ">
       <div className="h-full">
@@ -9,7 +12,12 @@ const AdminEditPromotion = () => {
       </div>
 
       <div className="flex-1 flex flex-col">
-        <EditPromotionNav />
+        <EditPromotionNav
+          buttonAdd="แก้ไข"
+          buttonCancel="ยกเลิก"
+          //handleEditPromotion={handleEditPromotion}
+          onClickButtonCancel={() => navigate("/admin-promotion")}
+        />
 
         <div className="flex-1 p-8  overflow-y-auto bg-base relative">
           <AddPromotionDetail />
