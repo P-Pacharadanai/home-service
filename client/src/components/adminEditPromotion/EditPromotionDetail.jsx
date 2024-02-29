640bcf15a408d2f53761776f8f1abf0cb122ba65
iHEDISNICE <hedis_nice@yahoo.com>
2024-02-29 11:21:13 +0700

    style : add UI

---
+++ client/src/components/adminEditPromotion/EditPromotionDetail.jsx
@@ -0,0 +1,225 @@
+import { useState, useEffect } from "react";
+import { DatePicker, TimePicker } from "antd";
+import dayjs from "dayjs";
+import "dayjs/locale/th";
+import "antd/lib/locale/th_TH";
+import {
+  pickerStyle,
+  fontStyle,
+  spanStyle,
+  divStyle,
+} from "./EditPromotionStyle";
+
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { convertThaiDateTime } from "../common";
+
+const EditPromotionDetail = (props) => {
+  const navigate = useNavigate();
+  const [categoryData, setCategoryData] = useState({});
+  const params = useParams();
+
+  const getCategoryData = async () => {
+    try {
+      const { data } = await axios.get(
+        `${import.meta.env.VITE_APP_HOME_SERVICE_API}/promotion/${
+          params.promotionId
+        }`
+      );
+      setCategoryData(data.data);
+      //console.log(data.data);
+    } catch (error) {
+      console.error("Failed to fetch category data:", error);
+    }
+  };
+
+  useEffect(() => {
+    getCategoryData();
+  }, []);
+
+  function formatDiscount(discount) {
+    if (typeof discount === "string" && discount.includes("%")) {
+      return `${discount}`;
+    } else if (typeof discount === "number") {
+      if (discount === 10) {
+        return `-${discount.toFixed(2)}%`;
+      } else {
+        return `-${discount.toFixed(2)}฿`;
+      }
+    } else {
+      return discount || "No discount";
+    }
+  }
+
+  const {
+    promotionCode,
+    setPromotionCode,
+    promotionType,
+    setPromotionType,
+    fixedDiscount,
+    setFixedDiscount,
+    percentDiscount,
+    setPercentDiscount,
+    discount,
+    setDiscount,
+    usageLimit,
+    setUsageLimit,
+    expirationDate,
+    expirationTime,
+    setExpirationDate,
+    setExpirationTime,
+  } = props;
+  const changeDate = (_, dateString) => {
+    setExpirationDate(dateString);
+  };
+
+  const changeTime = (_, timeString) => {
+    setExpirationTime(timeString);
+  };
+
+  const disabledDate = (current) => {
+    return current && current < dayjs().endOf("day");
+  };
+  console.log(discount);
+  return (
+    <div className="w-[1120px]  px-6 py-10 bg-white rounded-lg border border-gray-200 flex-col justify-start items-start gap-10 inline-flex font-['Prompt'] ">
+      <div className={divStyle}>
+        <p className={fontStyle}>Promotion Code</p>
+        <input
+          value={promotionCode}
+          onChange={(e) => setPromotionCode(e.target.value)}
+          className="w-[433px] h-11 px-4 py-2.5 bg-white rounded-lg border border-gray-300 justify-start items-center gap-2.5 flex  focus:outline-none "
+        />
+      </div>
+      <div className="w-[497px] h-24 justify-start items-start gap-6 inline-flex">
+        <p className={fontStyle}>ประเภท</p>
+        <div className="flex-col justify-start items-start gap-3 inline-flex ">
+          <div className="w-[268px] justify-between items-center inline-flex ">
+            <div className="justify-start items-center gap-3 flex  ">
+              <input
+                type="radio"
+                value="fixed"
+                checked={promotionType === "fixed"}
+                onChange={(e) => setPromotionType(e.target.value)}
+                className="w-5 h-5 justify-start items-start flex focus:outline-none "
+              />
+              <p
+                className={`text-black text-sm font-normal font-['Prompt'] leading-[21px] ${
+                  promotionType === "percent" ? "text-gray-400" : ""
+                }`}
+              >
+                Fixed
+              </p>
+            </div>
+            <div className="relative">
+              <input
+                type="number"
+                value={fixedDiscount}
+                placeholder="฿"
+                style={{ textAlign: "right" }}
+                onChange={(e) => {
+                  const newValue = e.target.value;
+                  setFixedDiscount(newValue);
+                  setDiscount(newValue);
+                }}
+                className={`w-[140px] h-[42px] px-[13px] py-[9px] bg-white rounded-md border border-gray-300 justify-end items-center flex focus:outline-none disabled-input ${
+                  promotionType === "percent" ? "disabled-input" : ""
+                }`}
+                disabled={promotionType === "percent"}
+              />
+            </div>
+          </div>
+
+          <div className="w-[268px] justify-between items-center inline-flex">
+            <div className="justify-start items-center gap-3 flex">
+              <input
+                type="radio"
+                value="percent"
+                checked={promotionType === "percent"}
+                onChange={(e) => setPromotionType(e.target.value)}
+                className="w-5 h-5 justify-start items-start flex "
+              />
+              <p
+                className={`text-black text-sm font-normal font-['Prompt'] leading-[21px] ${
+                  promotionType === "fixed" ? "text-gray-400" : ""
+                }`}
+                disabled={promotionType === "fixed"}
+              >
+                Percent
+              </p>
+            </div>
+            <div className="relative">
+              <input
+                type="number"
+                value={percentDiscount}
+                placeholder="%"
+                style={{ textAlign: "right" }}
+                onChange={(e) => {
+                  setPercentDiscount(e.target.value),
+                    setDiscount(e.target.value);
+                }}
+                className={`w-[140px] h-[42px] px-[13px] py-[9px] bg-white rounded-md border border-gray-300 justify-end items-center flex focus:outline-none disabled-input ${
+                  promotionType === "fixed" ? "disabled-input" : ""
+                }`}
+                disabled={promotionType === "fixed"}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={divStyle}>
+        <p className={fontStyle}>โควต้าการใช้</p>
+        <div>
+          <input
+            type="number"
+            value={usageLimit}
+            onChange={(e) => setUsageLimit(e.target.value)}
+            placeholder="ครั้ง"
+            style={{ textAlign: "right" }}
+            className=" flex w-[433px] h-11 px-4 py-2.5 bg-white rounded-lg border border-gray-300 gap-2.5 focus:outline-none"
+          />
+        </div>
+      </div>
+
+      <div className={divStyle}>
+        <p className={fontStyle}>วันหมดอายุ</p>
+        <DatePicker
+          format="DD/MM/YYYY"
+          locale="th_TH"
+          disabledDate={disabledDate}
+          showDate={{
+            defaultValue: dayjs().format("DD/MM/YYYY"),
+          }}
+          onChange={changeDate}
+          placeholder="กรุณาเลือกวันที่"
+          style={pickerStyle}
+        />
+        <TimePicker
+          format="HH:mm"
+          showNow={true}
+          showTime={{
+            defaultValue: dayjs("00:00", "HH:mm"),
+          }}
+          onChange={changeTime}
+          placeholder="กรุณาเลือกเวลา"
+          style={pickerStyle}
+        />
+      </div>
+      <hr className="border-t border-gray-300 w-full mb-2 mt-2" />
+      <div className="grid grid-cols-2 p-4 w-[850px] font-prompt">
+        <p className="font-prompt font-medium max-w-40">สร้างเมื่อ</p>
+        <p className="flex justify-start items-start -ml-32 text-black">
+          {convertThaiDateTime(categoryData.created_at)}
+        </p>
+      </div>
+      <div className="grid grid-cols-2 p-4 w-[850px] font-prompt">
+        <p className="font-medium font-prompt max-w-40 ">แก้ไขล่าสุด</p>
+        <p className="flex justify-start items-start -ml-32 text-black">
+          {convertThaiDateTime(categoryData.updated_at)}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default EditPromotionDetail;
--- client/src/pages/AdminEditPromotion.jsx
+++ client/src/pages/AdminEditPromotion.jsx
@@ -1,9 +1,19 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 import { SidebarNavAdmin } from "../components/common/";
 import { EditPromotionNav } from "../components/adminPromotion";
-import { AddPromotionDetail } from "../components/adminAddPromotion";
+import EditPromotionDetail from "../components/adminEditPromotion/EditPromotionDetail";
 
 const AdminEditPromotion = () => {
+  const [promotionCode, setPromotionCode] = useState(""); // State for promotion code
+  const [promotionType, setPromotionType] = useState(""); // State for promotion type
+  const [fixedDiscount, setFixedDiscount] = useState("");
+  const [percentDiscount, setPercentDiscount] = useState("");
+  const [discount, setDiscount] = useState("");
+  const [usageLimit, setUsageLimit] = useState(""); // State for usage limit
+  const [expirationDate, setExpirationDate] = useState(null); // State for expiration date
+  const [expirationTime, setExpirationTime] = useState(null); // State for expiration time
+
   const navigate = useNavigate();
   return (
     <div className="flex h-screen ">
@@ -20,7 +30,24 @@ const AdminEditPromotion = () => {
         />
 
         <div className="flex-1 p-8  overflow-y-auto bg-base relative">
-          <AddPromotionDetail />
+          <EditPromotionDetail
+            promotionCode={promotionCode}
+            setPromotionCode={setPromotionCode}
+            promotionType={promotionType}
+            setPromotionType={setPromotionType}
+            fixedDiscount={fixedDiscount}
+            setFixedDiscount={setFixedDiscount}
+            percentDiscount={percentDiscount}
+            setPercentDiscount={setPercentDiscount}
+            discount={discount}
+            setDiscount={setDiscount}
+            usageLimit={usageLimit}
+            setUsageLimit={setUsageLimit}
+            expirationDate={expirationDate}
+            expirationTime={expirationTime}
+            setExpirationDate={setExpirationDate}
+            setExpirationTime={setExpirationTime}
+          />
         </div>
       </div>
     </div>
