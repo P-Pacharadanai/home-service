29e3fd941d263b825ca377ac37e4fab16ceb3f1b
Hazuki <okubo.mae@gmail.com>
2024-03-02 01:12:23 +0900

    style: formatting

--- client/src/components/common/AlertModal.jsx
+++ client/src/components/common/AlertModal.jsx
@@ -6,11 +6,11 @@ const AlertModal = ({ isOpen, onClose, onConfirm, promoCode }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-      <div className="bg-white px-6 py-12 gap-4 rounded-xl shadow-lg flex flex-col justify-center items-center relative font-prompt">
+      <div className="bg-white px-14 py-12 gap-4 rounded-3xl shadow-xl flex flex-col justify-center items-center relative font-prompt">
         <button onClick={onClose} className="absolute top-0 right-0 mt-4 mr-4  hover:text-gray-600" aria-label="Close"> <X className="h-6 w-6" /></button>
         <img src={oopsIcon} />
         <h2 className="text-2xl">ยืนยันการลบรายการ?</h2>
-        <p className="text-xl text-gray-700">คุณต้องการลบรายการ ‘{promoCode.code}’ ใช่หรือไม่</p>
+        <p className="text-xl text-gray-700">คุณต้องการลบรายการ ‘{promoCode.code}’ <br/> <span className="flex justify-center items-center">ใช่หรือไม่</span></p>
         <div className="flex justify-center gap-4 mt-4">
           <button onClick={onConfirm} className="bg-blue-600 text-white px-4 py-2 rounded-lg border border-transparen hover:scale-110 duration-300 hover:ease-in">
             ลบรายการ
